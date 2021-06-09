aeea6a634091a301b83e4338b7a6283efce93ee7
Bezunesh <zueb21@gmail.com>
2021-06-09 05:26:27 -0400

    feat: Add categories view
    Changes the index view to show the list of categories.

--- userreviews/urls.py
+++ userreviews/urls.py
@@ -5,5 +5,6 @@ app_name='userreviews'
 urlpatterns = [
     path('', views.index, name='index'),
     path('restaurant/<int:restaurant_id>', views.restaurant, name='restaurant'),
+    path('category/<int:category_id>', views.category, name='category'),
     #path(r'^reviews/', include('reviews.urls')),
 ]
--- userreviews/views.py
+++ userreviews/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from .models import Restaurant
+from .models import Category, Restaurant
 # Create your views here.
 def index(request):
-    restaurants = Restaurant.objects.all()
-    return render(request, 'userreviews/index.html', {'restaurants': restaurants})
+    categories = Category.objects.all()
+    return render(request, 'userreviews/index.html', {'categories': categories})
+
+def category(request, category_id):
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'userreviews/category.html', {'category': category})
 
 def restaurant(request, restaurant_id):
     restaurant = Restaurant.objects.get(pk=restaurant_id)
