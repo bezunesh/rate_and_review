e18a61fc134d7f02ab48c55df4e0f1c8b0d83688
Bezunesh <zueb21@gmail.com>
2021-09-10 14:17:42 -0400

    feat: Created home ui

--- rateandreview/urls.py
+++ rateandreview/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from userreviews import views 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    #path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('reviews/', include('userreviews.urls')),
     path('reviews/', include('reviews.urls')),
--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 
 from . import views
 
-
+app_name="users"
 urlpatterns = [path('signup/', views.SignUp.as_view(), name='signup'), ]
