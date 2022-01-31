7cafd427f5c6b56ef17295b3cffa82faae1b7440
Bezunesh <zueb21@gmail.com>
2022-01-31 18:41:51 -0500

    fix: allow all hosts temporarily

--- rateandreview/settings.py
+++ rateandreview/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 INTERNAL_IPS = ('127.0.0.1',)
 
 # Application definition
