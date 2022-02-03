1449ecee679aa64cce6bc918e42a8ed5088831fd
Bezunesh <zueb21@gmail.com>
2022-02-03 11:48:42 -0500

    fix: Remove add category endpoint

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.getCategories),
-    path('add/', views.addCategory),
+    # path('add/', views.addCategory),
     path('category/<int:category_id>', views.getCategory),
     path('item/<int:item_id>', views.getItem),
     path('items/category/<int:category_id>', views.getItemsByCategory),
