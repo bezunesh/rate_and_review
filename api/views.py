ac61e3c85f7e8fd026102429dac58ccb5c14b609
Bezunesh <zueb21@gmail.com>
2022-02-02 22:37:38 -0500

    feat: Adds item serializer and api endpoints

--- api/views.py
+++ api/views.py
@@ -1,12 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializers import CategorySerialzer
-from userreviews.models import Category
+from .serializers import CategorySerialzer, ItemSerializer
+from userreviews.models import Category, Item
 
 @api_view(['GET'])
 def getCategories(request):
     categories = Category.objects.all()
     serializer = CategorySerialzer(categories, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getCategory(request, category_id):
+    category = Category.objects.get(id=category_id)
+    serializer = CategorySerialzer(category)
     return Response(serializer.data)
 
 @api_view(['POST'])
@@ -15,3 +21,20 @@ def addCategory(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['GET'])
+def getItemsByCategory(request, category_id):
+    items = Item.objects.filter(category=category_id)
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getItem(request, item_id):
+    item = Item.objects.get(id=item_id)
+    serializer = ItemSerializer(item)
+    return Response(serializer.data)
+
+# todo: getReviews for an item
+# todo: getRatings for an item
+
+    
