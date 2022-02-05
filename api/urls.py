2fac5df0bfd69c6c22473e3e83da4f0ab3094644
Bezunesh <zueb21@gmail.com>
2022-02-04 23:31:00 -0500

    feat: Adds class-based API views

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getCategories),
-    # path('add/', views.addCategory),
-    path('category/<int:category_id>', views.getCategory),
-    path('item/<int:item_id>', views.getItem),
-    path('items/category/<int:category_id>', views.getItemsByCategory),
-    path('item/reviews/<int:item_id>', views.getReviews),
+    path('categories/', views.CategoryList.as_view()),
+    path('add/category/', views.CategoryList.as_view()),
+    path('category/<int:category_id>', views.CategoryDetail.as_view()),
+    path('item/<int:item_id>', views.ItemDetail().as_view()),
+    path('items/category/<int:category_id>', views.ItemList.as_view()),
+    path('item/reviews/<int:item_id>', views.ItemReviews.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,44 +1,72 @@
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .serializers import CategorySerialzer, ItemSerializer, ReviewSerializer
 from userreviews.models import Category, Item
 from reviews.models import Review
+from django.http import Http404
 
-@api_view(['GET'])
-def getCategories(request):
-    categories = Category.objects.all()
-    serializer = CategorySerialzer(categories, many=True)
-    return Response(serializer.data)
+class CategoryList(APIView):
+    """
+    List all categories or create a category
+    """
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerialzer(categories, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CategorySerialzer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-def getCategory(request, category_id):
-    category = Category.objects.get(id=category_id)
-    serializer = CategorySerialzer(category)
-    return Response(serializer.data)
+class CategoryDetail(APIView):
+    """
+    Retrive, update or delete a category
+    """
+    def get(self, request, category_id):
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            raise Http404
+        serializer = CategorySerialzer(category)
+        return Response(serializer.data)
 
-@api_view(['POST'])
-def addCategory(request):
-    serializer = CategorySerialzer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
 
-@api_view(['GET'])
-def getItemsByCategory(request, category_id):
-    items = Item.objects.filter(category=category_id)
-    serializer = ItemSerializer(items, many=True)
-    return Response(serializer.data)
+class ItemList(APIView):
+    """ List all items of a single category """
+    def get(self, request, category_id):
+        items = Item.objects.filter(category=category_id)
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
 
-@api_view(['GET'])
-def getItem(request, item_id):
-    item = Item.objects.get(id=item_id)
-    serializer = ItemSerializer(item)
-    return Response(serializer.data)
 
-@api_view(['GET'])
-def getReviews(request, item_id):
-    item = Item.objects.get(id=item_id)
-    reviews = item.reviews.all()
-    serializer = ReviewSerializer(reviews, many=True)
-    return Response(serializer.data)
+def getItemByPk(pk):
+        try:
+            return Item.objects.get(pk=pk)
+        except Item.DoesNotExist:
+            raise Http404
+
+class ItemDetail(APIView):
+    """ 
+    Retreive, add, update or delete an item.
+    """
+    def get(self, request, item_id):
+        item = getItemByPk(tem_id)
+        serializer = ItemSerializer(item)
+        return Response(serializer.data)
+
+    
+class ItemReviews(APIView):
+    """ 
+    Retrive, add, update of delete item reviews
+    """
+    def get(self, request, item_id):
+        item = getItemByPk(item_id)
+        reviews = item.reviews.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
     
