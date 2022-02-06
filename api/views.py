6ab22e499334c271e27a0b7164e556e534a546cd
Bezunesh <zueb21@gmail.com>
2022-02-05 23:08:23 -0500

    feat: improves views using generic class based views

--- api/views.py
+++ api/views.py
@@ -6,68 +6,46 @@ from rest_framework import mixins, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-class CategoryList(mixins.ListModelMixin, 
-                   mixins.CreateModelMixin,
-                   generics.GenericAPIView):
+class CategoryList(generics.ListCreateAPIView):
     """
     List all categories or create a category
     """
     queryset = Category.objects.all()
     serializer_class = CategorySerialzer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class CategoryDetail(mixins.RetrieveModelMixin, 
-                     generics.GenericAPIView):
+class CategoryDetail(generics.RetrieveAPIView):
     """
     Retrive, update or delete a category
     """
     queryset = Category.objects.all()
     serializer_class = CategorySerialzer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-
-class ItemList(mixins.ListModelMixin, 
-               generics.GenericAPIView):
+class ItemList(generics.ListAPIView):
     """ List all items of a single category """
     serializer_class = ItemSerializer
+    
+    def get_queryset(self):
+        return Item.objects.filter(category_id = self.kwargs['category_id'])
 
-    def get(self, request, *args, **kwargs):
-        self.queryset = Item.objects.filter(category_id = kwargs['category_id'])
-        return self.list(request, *args, **kwargs)
-
-class ItemDetail(mixins.RetrieveModelMixin,
-                 generics.GenericAPIView):
+class ItemDetail(generics.RetrieveAPIView):
     """ 
     Retreive, add, update or delete an item.
     """
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-class ItemReviews(mixins.ListModelMixin, 
-                  generics.GenericAPIView):
+class ItemReviews(generics.ListAPIView):
     """
     List, add, update or delete review of an item.
     """
     serializer_class = ReviewSerializer
 
-    def get_object(self, pk):
+    def get_queryset(self):
         try:
-            return Item.objects.get(pk=pk)
+            item = Item.objects.get(pk=self.kwargs['pk'])
         except Item.DoesNotExist:
             raise Http404
-
-    def get(self, request, *args, **kwargs):
-        item = self.get_object(kwargs['pk'])
-        self.queryset = item.reviews.all()
-        return self.list(request, *args, **kwargs)
+        return item.reviews.all()
     
